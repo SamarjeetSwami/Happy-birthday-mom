39660cb1909082307c48981e48d25960c8071a35
SamarjeetSwami <104068237+SamarjeetSwami@users.noreply.github.com>
2022-12-15 16:31:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.js.fromUrl('Birthdayimage.jpg', function(Img) {
+        block_image_object = Img;
+        block_image_object.scaleToWidth(700);
+        block_image_object.scaleToHeight(510);
+        block_image_object.set({
+            top:0,
+            left:
+        })
+
+    });
+
+
+
+
+
+	
+
+
+});
+
+function playSound(){
+
+}
